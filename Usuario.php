4b8fb4c2dce6b313232ec81425f65f13fdfd9c41
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2022-02-18 10:52:26 -0300

    feat: adiciona exceptions nos metodos

--- Usuario.php
+++ Usuario.php
@@ -24,35 +24,56 @@ class Usuario
 
     public function logar($login, $senha)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function cadastrar($login, $senha)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function logout($usuarioId)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function alterarDados($usuarioId, $login = null, $senha = null)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function ehAdministrador($usuarioId)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function ehModerador($usuarioId)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function adicionarALista($usuarioId, $listaId)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function removerDaLista($usuarioId, $listaId)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function adicionarNota($usuarioId, $conteudo)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function editarNota($notaId, $usuarioId, $conteudo)
     {
+        throw new \Exception('Não implementado ainda!');
     }
+
     public function removerNota($notaId)
     {
+        throw new \Exception('Não implementado ainda!');
     }
 }
